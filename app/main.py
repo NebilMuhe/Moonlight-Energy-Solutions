0b03b0080fcecc7b083cd819e03253b41f0d4c65
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-12-08 09:00:03 +0300

    cleaned the data

--- app/main.py
+++ app/main.py
@@ -2,8 +2,13 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.data_preparation import load_data
+
+from src.data_preparation import load_data,clean_data
 
 
 if __name__ == '__main__':
-    load_data('data/benin-malanville.csv')
+    data = load_data('data/benin-malanville.csv')
+
+    data = clean_data(data)
+    
+    
--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,10 +1,21 @@
 import pandas as pd
 from scipy.stats import zscore
 
-def load_data(file_path): 
+
+def load_data(file_path):
     data = pd.read_csv(file_path)
     data['Timestamp'] = pd.to_datetime(data['Timestamp'])
-    print(data)
+    print(data.shape)
     return data
 
 
+def clean_data(data):
+    cleaned_data = data.dropna(subset=['Timestamp'])
+
+    if cleaned_data['Comments'].isna().all():
+        cleaned_data = cleaned_data.drop(columns=['Comments'])
+
+    numeric_cols = cleaned_data.select_dtypes(
+        include=['int64', 'float64']).columns
+    cleaned_data[numeric_cols] = cleaned_data[numeric_cols].apply(zscore)
+    return data
